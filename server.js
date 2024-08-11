5459796075a6e740dd967f64041588e0467bcfc3
zekud <zekudk@gmail.com>
2024-08-11 22:41:00 +0300

    chore: Update server.js and cronService.js

--- server.js
+++ server.js
@@ -11,11 +11,10 @@ const { termModel } = require("./models");
 const { any } = require("joi");
 const { RoomTypes, SubRooms } = require("./models/rooms.schema");
 const { overnightBooking } = require("./models/overnight.booking.schema");
-require("./services/cronService");
 const app = express();
 const port = 4000 || 4001;
 connectDatabase();
-
+require("./services/cronService");
 app.use(express.json());
 
 app.use(cors({ origin: "*" }));
@@ -25,21 +24,17 @@ app.use(errorMiddleware); // CUSTOM ERROR MIDDLEWARE
 
 app.get("/health", function (req, res) {
   return res.send("Server Operation Success");
- });
-
+});
 
 app.use(errorMiddleware);
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // CUSTOM ERROR MIDDLEWARE
 
 app.use("/api/v1", allRoutes); // ALL API END POINTS
 
-
 // INAVLID API CALL
 app.use((req, res, next) => {
   next(new ErrorResponse("Invalid Api", statusCode?.notFound));
 });
-
-
 
 app.listen(port, () => {
   console.log(`server is running on PORT ${port}`);
