542e5c73a05c1884072075b801f27089fe72baf0
zekud <zekudk@gmail.com>
2024-08-20 22:29:44 +0300

    room configuration

--- routes/room.router.js
+++ routes/room.router.js
@@ -1,10 +1,10 @@
-const {create,getAll, del} = require('../services/room.service')
+const { create, getAll, getByType, del } = require("../services/room.service");
 
-const router = require('express').Router()
+const router = require("express").Router();
 
+router.post("/create", create);
+router.get("/get", getAll);
+router.get("/get/:type", getByType);
+router.delete("/delete/:id", del);
 
-router.post("/create",create)
-router.get("/get/:type",getAll)
-router.delete("/delete/:id",del)
-
-module.exports = router
+module.exports = router;
--- services/room.service.js
+++ services/room.service.js
@@ -1,39 +1,52 @@
-const { asyncErrorHandler, ErrorResponse } = require("../middlewares/error/error");
+const {
+  asyncErrorHandler,
+  ErrorResponse,
+} = require("../middlewares/error/error");
 const { roomModel } = require("../models");
 const { statusCode } = require("../utils/statusCode");
 
-
-const create = asyncErrorHandler(async(req,res)=>{
-    if(req.body.id.length>0){
-        let update = await roomModel.findByIdAndUpdate(req.body.id,req.body)
-        if(update){
-            res.status(statusCode.accepted).json(update)
-        }
+const create = asyncErrorHandler(async (req, res) => {
+  if (req.body.id.length > 0) {
+    let update = await roomModel.findByIdAndUpdate(req.body.id, req.body);
+    if (update) {
+      res.status(statusCode.accepted).json(update);
     }
-    else{
-        let room = await roomModel.create(req.body)
-        if(room){
-            res.status(statusCode.accepted).json(room)
-        }
-        else{
-            throw new ErrorResponse("Failed To Create Room",500)
-        }
+  } else {
+    let room = await roomModel.create(req.body);
+    if (room) {
+      res.status(statusCode.accepted).json(room);
+    } else {
+      throw new ErrorResponse("Failed To Create Room", 500);
     }
-})
+  }
+});
 
-const getAll = asyncErrorHandler(async(req,res)=>{
-    let allDaypass = await roomModel.find({type:req.params.type})
-    if(allDaypass.length>0) {res.status(statusCode.accepted).json(allDaypass)}
-    else {throw new ErrorResponse("No Room Found",404)}
-})
+const getByType = asyncErrorHandler(async (req, res) => {
+  let allDaypass = await roomModel.find({ type: req.params.type });
 
+  if (allDaypass.length > 0) {
+    res.status(statusCode.accepted).json(allDaypass);
+  } else {
+    throw new ErrorResponse("No Room Found", 404);
+  }
+});
 
-const del = asyncErrorHandler(async(req,res)=>{
-    let allDaypass = await roomModel.findByIdAndDelete(req.param.id)
-    if(allDaypass) {res.status(holidayModel.accepted).json({msg:"DELETED"})}
-    else {throw new ErrorResponse("No Payment Found",404)}
-})
+const getAll = asyncErrorHandler(async (req, res) => {
+  let allDaypass = await roomModel.find();
+  if (allDaypass.length > 0) {
+    res.status(statusCode.accepted).json(allDaypass);
+  } else {
+    throw new ErrorResponse("No Room Found", 404);
+  }
+});
 
+const del = asyncErrorHandler(async (req, res) => {
+  let allDaypass = await roomModel.findByIdAndDelete(req.param.id);
+  if (allDaypass) {
+    res.status(holidayModel.accepted).json({ msg: "DELETED" });
+  } else {
+    throw new ErrorResponse("No Payment Found", 404);
+  }
+});
 
-
-module.exports = { create, getAll,del}
+module.exports = { create, getByType, getAll, del };
